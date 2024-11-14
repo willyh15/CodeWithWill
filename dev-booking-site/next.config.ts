0126740f3cf72524e38b31590f271ce499503e77
Will Harrison <willharrison2016@gmail.com>
2024-11-14 19:15:49 +0000

    up

--- dev-booking-site/next.config.ts
+++ dev-booking-site/next.config.ts
@@ -1,8 +1,11 @@
-// next.config.js
-module.exports = {
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   output: 'export',  // Ensures Next.js exports static HTML files
   images: {
     unoptimized: true,  // Disable Next.js image optimization for static export
   },
   trailingSlash: true, // Ensures each page generates its own folder with index.html
 };
+
+export default nextConfig;
