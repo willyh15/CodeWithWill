802a14077acc85efe002cb0ee1c61a2462bd58dd
Will Harrison <willharrison2016@gmail.com>
2024-11-14 19:32:52 +0000

    up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,13 @@
 import type { Config } from "tailwindcss";
 
-export default {
+export default <Config>{
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-} satisfies Config;
+};
