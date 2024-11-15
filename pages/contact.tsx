a80b968200905a5877b49c17605a135116127bb9
Will Harrison <willharrison2016@gmail.com>
2024-11-15 10:25:20 -0600

    Update contact.tsx

---
+++ pages/contact.tsx
@@ -0,0 +1,11 @@
+import ContactForm from "@/components/ContactForm";
+import Footer from "@/components/Footer";
+
+export default function ContactPage() {
+  return (
+    <>
+      <ContactForm />
+      <Footer />
+    </>
+  );
+}
