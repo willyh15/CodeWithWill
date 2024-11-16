29c9feff058c9c48a2da06cdb618d9e4179d1bca
Will Harrison <willharrison2016@gmail.com>
2024-11-16 16:28:53 -0600

    Update and rename fetchBackground.ts to fetchBackground.js

---
+++ utils/fetchBackground.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+async function fetchBackground(query = "abstract") {
+  const apiKey = process.env.PEXELS_API_KEY;
+  const endpoint = `https://api.pexels.com/v1/search?query=${query}&per_page=1`;
+
+  try {
+    const response = await axios.get(endpoint, {
+      headers: {
+        Authorization: apiKey,
+      },
+    });
+
+    const photos = response.data.photos;
+    if (photos.length > 0) {
+      return photos[0].src.landscape; // Return the landscape version of the image
+    } else {
+      throw new Error("No images found.");
+    }
+  } catch (error) {
+    console.error("Error fetching background image:", error);
+    throw error;
+  }
+}
+
+module.exports = { fetchBackground };
