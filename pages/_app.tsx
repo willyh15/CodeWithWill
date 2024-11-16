b95d86da9d795d38b7565ed8c4f757e4cb37274f
Will Harrison <willharrison2016@gmail.com>
2024-11-16 00:02:14 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-// pages/_app.tsx
 import "../global.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
