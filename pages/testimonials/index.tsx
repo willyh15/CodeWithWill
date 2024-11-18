0858ffdb41f8d86b26a6520cd5170093a5bf6363
Will Harrison <willharrison2016@gmail.com>
2024-11-18 14:21:21 -0600

    Update index.tsx

---
+++ pages/testimonials/index.tsx
@@ -0,0 +1,9 @@
+import Testimonials from "@/components/Testimonials/Testimonials";
+
+export default function TestimonialsPage() {
+  return (
+    <div className="max-w-6xl mx-auto py-10 px-6">
+      <Testimonials />
+    </div>
+  );
+}
