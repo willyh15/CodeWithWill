de9d982c15a9fd043072d66a789fd618291afb1a
Will Harrison <willharrison2016@gmail.com>
2024-11-18 13:58:13 -0600

    Update index.tsx

---
+++ pages/projects/index.tsx
@@ -0,0 +1,9 @@
+import Projects from "@/components/Projects/Projects";
+
+export default function ProjectsPage() {
+  return (
+    <div className="max-w-6xl mx-auto py-10 px-6">
+      <Projects />
+    </div>
+  );
+}
