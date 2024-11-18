58747c0f7ca53081386feb725300c14a535e2563
Will Harrison <willharrison2016@gmail.com>
2024-11-18 13:34:41 -0600

    Update index.tsx

---
+++ pages/contact/index.tsx
@@ -0,0 +1,9 @@
+import ContactForm from "@/components/Contact/ContactForm";
+
+export default function ContactPage() {
+  return (
+    <div className="max-w-4xl mx-auto py-10 px-6">
+      <ContactForm />
+    </div>
+  );
+}
