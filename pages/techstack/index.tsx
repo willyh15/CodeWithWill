fefa3dd7fa7f3c6fa64da25eac5cf4635f7765c4
Will Harrison <willharrison2016@gmail.com>
2024-11-18 14:24:14 -0600

    Update index.tsx

---
+++ pages/techstack/index.tsx
@@ -0,0 +1,9 @@
+import TechStack from "@/components/TechStack/TechStack";
+
+export default function TechStackPage() {
+  return (
+    <div className="max-w-6xl mx-auto py-10 px-6">
+      <TechStack />
+    </div>
+  );
+}
