d2a94829c580e2a9eab3cecf1d08b8ced9a4bb28
Will Harrison <willharrison2016@gmail.com>
2024-11-18 13:59:14 -0600

    Update index.tsx

---
+++ pages/skills/index.tsx
@@ -0,0 +1,9 @@
+import Skills from "@/components/Skills/Skills";
+
+export default function SkillsPage() {
+  return (
+    <div className="max-w-6xl mx-auto py-10 px-6">
+      <Skills />
+    </div>
+  );
+}
