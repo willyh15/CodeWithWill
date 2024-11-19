5c3753439dae92e5608ebeb343613caeed988e95
Will Harrison <willharrison2016@gmail.com>
2024-11-19 04:36:53 +0000

    up

--- next.config.js
+++ next.config.js
@@ -8,31 +8,6 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; img-src 'self' data: https://images.pexels.com;",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains; preload",
-          },
-        ],
-      },
-    ];
-  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
