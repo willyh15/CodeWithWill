0ec1bc9dcd4c198b5185a20294584ce80558d5b6
Will Harrison <willharrison2016@gmail.com>
2024-11-24 08:31:30 +0000

    up

--- next.config.js
+++ next.config.js
@@ -6,7 +6,29 @@ const nextConfig = {
     domains: ["images.pexels.com"],
   },
   trailingSlash: true,
-  output: "export", // Use 'export' instead of 'next export'
+  output: "export",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              connect-src 'self' https://api.pexels.com;
+              img-src 'self' https://images.pexels.com data:;
+              style-src 'self' 'unsafe-inline';
+            `.trim(),
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
