a564eb5cffe38b090c5d4318c1150f8f8225ece6
Will Harrison <willharrison2016@gmail.com>
2024-11-24 07:39:11 +0000

    up

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const nextConfig = {
-    env: {
-        PEXELS_API_KEY: process.env.PEXELS_API_KEY,
-          },
-            images: {
-                domains: ["images.pexels.com"],
-                  },
-                    trailingSlash: true,
-                    };
+  env: {
+    PEXELS_API_KEY: process.env.PEXELS_API_KEY, // This injects the API key into the build
+  },
+  images: {
+    domains: ["images.pexels.com"], // Allow Pexels image domains
+  },
+  trailingSlash: true,
+};
 
-                    export default nextConfig;
+export default nextConfig;
