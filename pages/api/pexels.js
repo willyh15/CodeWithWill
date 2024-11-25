35aec4c5c0869443139e47c7ad963c7ff8cd77ee
Will Harrison <willharrison2016@gmail.com>
2024-11-25 15:28:49 +0000

    up

---
+++ pages/api/pexels.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+    const API_URL = 'https://api.pexels.com/v1/search?query=abstract&per_page=1';
+    const PEXELS_API_KEY = process.env.PEXELS_API_KEY;
+  
+    try {
+      const response = await fetch(API_URL, {
+        headers: {
+          Authorization: PEXELS_API_KEY,
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`API Error: ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+  
