a948795cb60452354d34ff50410b835fd4bbbdd3
Will Harrison <willharrison2016@gmail.com>
2024-11-30 00:52:43 -0600

    Update fetchBackground.ts
    Signed-off-by: Will Harrison <willharrison2016@gmail.com>

--- utils/fetchBackground.ts
+++ utils/fetchBackground.ts
@@ -1,20 +1,10 @@
-export const fetchBackground = async (query: string) => {
-  const API_URL = "https://api.pexels.com/v1/search";
-  const PEXELS_API_KEY = process.env.PEXELS_API_KEY;
-
-  const url = new URL(API_URL);
-  url.searchParams.append("query", query);
-  url.searchParams.append("per_page", "1");
-
-  const response = await fetch(url.toString(), {
-    headers: {
-      Authorization: PEXELS_API_KEY as string,
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error(`Pexels API Error: ${response.statusText}`);
+export const fetchBackground = async () => {
+  try {
+    const response = await fetch("/api/pexels");
+    if (!response.ok) throw new Error("Failed to fetch background image");
+    return await response.json();
+  } catch (error) {
+    console.error("Fetch Background Error:", error);
+    return null;
   }
-
-  return await response.json();
 };
