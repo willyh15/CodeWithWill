598300599a1a3e5e4a9c236f58460fa47322ec60
Will Harrison <willharrison2016@gmail.com>
2024-11-30 00:58:14 -0600

    Update pexels.ts
    Signed-off-by: Will Harrison <willharrison2016@gmail.com>

--- pages/api/pexels.ts
+++ pages/api/pexels.ts
@@ -1,12 +1,17 @@
-export default async function handler(req, res) {
-  const query = req.query.query || "abstract"; // Default to "abstract" if no query is provided
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const query = (req.query.query as string) || "abstract"; // Default to "abstract" if no query is provided
   const API_URL = `https://api.pexels.com/v1/search?query=${query}&per_page=1`;
   const PEXELS_API_KEY = process.env.PEXELS_API_KEY;
 
   try {
     const response = await fetch(API_URL, {
       headers: {
-        Authorization: PEXELS_API_KEY,
+        Authorization: PEXELS_API_KEY || "", // Ensure the API key is provided
       },
     });
 
@@ -18,6 +23,6 @@ export default async function handler(req, res) {
     res.status(200).json({ background: data.photos[0]?.src?.original || "" });
   } catch (error) {
     console.error("API Error:", error);
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: (error as Error).message });
   }
 }
