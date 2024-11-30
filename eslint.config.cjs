26680845a967cefcf6f8b2b624210934baef6ae4
Will Harrison <willharrison2016@gmail.com>
2024-11-30 00:06:59 -0600

    Update eslint.config.cjs
    Signed-off-by: Will Harrison <willharrison2016@gmail.com>

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,23 +1,24 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "next/core-web-vitals",
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+const globals = require("globals");
+
+module.exports = [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Replaces `env.browser: true`
+        ...globals.es2021, // Replaces `env.es2021: true`
+      },
+      ecmaVersion: "latest", // Same as `ecmaVersion: 2021`
+      sourceType: "module", // Specifies ES module system
+      ecmaFeatures: {
+        jsx: true, // Enables JSX
+      },
     },
-    ecmaVersion: 2021,
-    sourceType: "module",
+    plugins: {
+      react: require("eslint-plugin-react"),
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      // Add your custom rules here
+    },
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {
-    // Add your custom rules here
-  },
-};
+];
