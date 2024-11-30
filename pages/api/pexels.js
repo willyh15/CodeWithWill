1e0530f87e266581f0691e12c9b48ad088c9cafd
Will Harrison <willharrison2016@gmail.com>
2024-11-30 00:46:50 -0600

    Update pexels.js
    Signed-off-by: Will Harrison <willharrison2016@gmail.com>

--- pages/api/pexels.js
+++ pages/api/pexels.js
@@ -16,7 +16,6 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    console.error("Error fetching Pexels API:", error.message);
     res.status(500).json({ error: error.message });
   }
 }
