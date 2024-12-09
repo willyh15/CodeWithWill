b3174908ae2b6b3be40c4314060c162809e94a4b
Will Harrison <willharrison2016@gmail.com>
2024-12-08 20:21:04 -0600

    Update BookingCenter.tsx
    Signed-off-by: Will Harrison <willharrison2016@gmail.com>

--- components/Booking/BookingCenter.tsx
+++ components/Booking/BookingCenter.tsx
@@ -24,7 +24,7 @@ export const BookingCenter = () => {
     setLoading(true);
     try {
       const { data, error } = await supabase
-        .from<Booking, "id">("bookings") // Provide both row type and primary key type
+        .from<Booking>("bookings") // Only pass the row type here
         .select("*");
       if (error) throw error;
       if (data) setBookings(data); // Ensure data is not null
