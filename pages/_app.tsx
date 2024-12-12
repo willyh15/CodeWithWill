71d969177103fde9c4f44d71f678e4ef9617c3fa
Will Harrison <willharrison2016@gmail.com>
2024-12-12 08:21:28 -0600

    Update _app.tsx
    Signed-off-by: Will Harrison <willharrison2016@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import "../styles/global.css";
 import type { AppProps } from "next/app";
 import { GlobalStateProvider } from "@/lib/globalState";
 import GlobalLayout from "@/components/Layout/GlobalLayout";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GlobalStateProvider>
-      <GlobalLayout>
-        <Component {...pageProps} />
-      </GlobalLayout>
+      <ErrorBoundary>
+        <GlobalLayout>
+          <Component {...pageProps} />
+        </GlobalLayout>
+      </ErrorBoundary>
     </GlobalStateProvider>
   );
 }
