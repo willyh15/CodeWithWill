39f478d65dd3fc7f108f63a535d928c464b31f83
Will Harrison <willharrison2016@gmail.com>
2024-12-29 14:44:52 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
 import "../styles/global.css";
 import type { AppProps } from "next/app";
-import { GlobalStateProvider } from "@/lib/globalState";
 import GlobalLayout from "@/components/Layout/GlobalLayout";
 import ErrorBoundary from "@/components/ErrorBoundary";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <GlobalStateProvider>
-      <ErrorBoundary>
-        <GlobalLayout>
-          <Component {...pageProps} />
-        </GlobalLayout>
-      </ErrorBoundary>
-    </GlobalStateProvider>
+    <ErrorBoundary>
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
+    </ErrorBoundary>
   );
 }
 
