10be37ed13f806dc3edbf761edc186cef3ac2a73
Will Harrison <willharrison2016@gmail.com>
2024-12-28 23:55:52 -0600

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -12,16 +12,13 @@ module.exports = {
   plugins: {
     react: require("eslint-plugin-react"),
     "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
-    next: require("eslint-plugin-next"), // Ensure this is correctly included
+    next: require("eslint-plugin-next"), // Ensure this is included
   },
   rules: {
     "no-unused-vars": "warn",
     "no-console": "off",
     "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
     "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
     "next/no-html-link-for-pages": "off",
   },
 };
