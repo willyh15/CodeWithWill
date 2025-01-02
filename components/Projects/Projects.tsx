8e6866cf652c46b3b403f801bdb27cc6d923bce1
Will Harrison <willharrison2016@gmail.com>
2025-01-02 02:40:02 -0600

    Update Projects.tsx

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import StarfieldCanvas from "@/components/Starfield/Starfield";
 
-export function Projects() {
+export default function Projects() {
   const projects = [
     {
       title: "E-Commerce Platform",
