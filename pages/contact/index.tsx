1fc23af1501db3946ffa8f580bff44edcb1f1391
Will Harrison <willharrison2016@gmail.com>
2025-01-03 21:29:44 -0600

    Update index.tsx

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,9 +1,11 @@
-import ContactForm from "@/components/Contact/ContactForm";
+import dynamic from "next/dynamic";
+
+const DynamicContactForm = dynamic(() => import("@/components/Contact/ContactForm"), { ssr: false });
 
 export default function ContactPage() {
   return (
     <div className="max-w-4xl mx-auto py-10 px-6">
-      <ContactForm />
+      <DynamicContactForm />
     </div>
   );
 }
