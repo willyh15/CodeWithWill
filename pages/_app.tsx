5ae989c800820418cb4ba5d83d04a57a1ea8b454
Will Harrison <willharrison2016@gmail.com>
2025-01-10 13:58:19 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,16 @@
-import "../styles/global.css"; // Global styles
-import "../styles/datepicker.css"; // Ensure relative path is correct
+import "../styles/global.css";
+import "../styles/calendarStyles.css"; // Import calendar styling
 import type { AppProps } from "next/app";
 import GlobalLayout from "@/components/Layout/GlobalLayout";
 import ErrorBoundary from "@/components/ErrorBoundary";
-import Head from "next/head";
-import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    console.log("App initialized.");
-  }, []);
-
   return (
-    <>
-      <Head>
-        <title>Code With Will</title>
-        <meta name="description" content="Code With Will - A futuristic app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ErrorBoundary>
-        <GlobalLayout>
-          <Component {...pageProps} />
-        </GlobalLayout>
-      </ErrorBoundary>
-    </>
+    <ErrorBoundary>
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
+    </ErrorBoundary>
   );
 }
 
